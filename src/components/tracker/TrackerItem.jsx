02dfd428e3af6211b78af9746c4cd79b2a7c824e
Your Name <you@example.com>
2023-12-07 08:54:14 +0100

    Comment out css file in one component to try and resolve Netlify issue

--- src/components/tracker/TrackerItem.jsx
+++ src/components/tracker/TrackerItem.jsx
@@ -1,5 +1,5 @@
-import { LottieCheckComponent, LottieXComponent } from "../LottieAnimations/LottieConsolidated";
-import "./Tracker.css";
+
+//import "./Tracker.css";
 
 
 export const TrackerItem = ({ entry }) => {
